a7552e3eecfdd5c61f2cda023c6d6ff99f88cf17
vivisuke <ntsuda@master.email.ne.jp>
2021-08-02 10:03:54 +0900

    0042 ◎ C 08/02 08/02 0.001│ToDo│【問題を作る】押下 → メインシーンに遷移

--- SakuSakuLogic20/LevelScene.gd
+++ SakuSakuLogic20/LevelScene.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var g = get_node("/root/Global")
 
+var dialog_opened = false
+var mouse_pushed = false
+var mouse_pos
+var scroll_pos
 
-# Called when the node enters the scene tree for the first time.
+var QuestPanel = load("res://QuestPanel.tscn")
+
+class MyCustomSorter:
+	var g 
+	static func sort_ascending(a, b):
+		return true if a[1] < b[1] else false
+	#static func sort_descending(a, b):
+	#	return true if a > b else false
 func _ready():
 	pass # Replace with function body.
 
@@ -14,3 +23,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_ClearButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_EditButton_pressed():
+	g.lvl_vscroll = $ScrollContainer.scroll_vertical
+	print("vscroll = ", g.lvl_vscroll)
+	g.solveMode = false;
+	get_tree().change_scene("res://MainScene.tscn")
+	pass # Replace with function body.
